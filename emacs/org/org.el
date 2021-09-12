f82382afae1aeaa9c0e056ccb58c8c9128ef7a41
James Ni <admacro@gmail.com>
2021-09-12 19:58:10 +0800

    convert space to tab

--- emacs/org/org.el
+++ emacs/org/org.el
@@ -15,18 +15,18 @@
 (setq org-publish-project-alist
       (list (list "notes"
                   :base-directory (concat org-project-notes-path "/org/")
-                  :base-extension "org"
-                  :publishing-directory org-project-notes-path
-                  :recursive t
-                  :publishing-function 'org-html-publish-to-html
-                  :section-numbers nil)
-            (list "www"
-                  :base-directory (concat org-project-www-path "/org/")
-                  :base-extension "org"
-                  :publishing-directory org-project-www-path
-                  :publishing-function 'org-html-publish-to-html
-                  :recursive t
-                  :section-numbers nil)
-            (list org-project-notes-name :components '("notes"))
-            (list org-project-www-name :components '("www"))
-            ))
+				  :base-extension "org"
+				  :publishing-directory org-project-notes-path
+				  :recursive t
+				  :publishing-function 'org-html-publish-to-html
+				  :section-numbers nil)
+			(list "www"
+				  :base-directory (concat org-project-www-path "/org/")
+				  :base-extension "org"
+				  :publishing-directory org-project-www-path
+				  :publishing-function 'org-html-publish-to-html
+				  :recursive t
+				  :section-numbers nil)
+			(list org-project-notes-name :components '("notes"))
+			(list org-project-www-name :components '("www"))
+			))
