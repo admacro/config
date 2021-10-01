687f7dcf10a0797c8f41d6540430efeaf5b541c3
James Ni <admacro@gmail.com>
2021-10-01 17:39:35 +0800

    add test lang files and reorganize folder

---
+++ emacs/plist & icns/langtest/test.java
@@ -0,0 +1,5 @@
+public abstract class BaseClass {
+    void basePublicMethod() {
+        System.out.println("BaseClass#basePublicMethod()");
+    }
+}
---
+++ emacs/plist & icns/langtest/test.js
@@ -0,0 +1,10 @@
+const myObject = {
+  firstName:"John",
+  lastName: "Doe",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+// This will return "John Doe":
+myObject.fullName();  
