dd1bb72572ae9bc1ecbde65ccdae4f1f042fc041
James Ni <admacro@gmail.com>
2022-10-21 16:04:39 +0800

    backup xah-find

---
+++ emacs/xah-find/xah-find-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from xah-find.el  -*- no-byte-compile: t -*-
+(define-package "xah-find" "20210925.1648" "find replace in pure emacs lisp. Purpose similar to grep/sed." '((emacs "24.1")) :commit "4ac27d806b17d646cb46167da30b8163d31d5073" :authors '(("Xah Lee ( http://xahlee.info/ )")) :maintainer '("Xah Lee ( http://xahlee.info/ )") :keywords '("convenience" "extensions" "files" "tools" "unix") :url "http://ergoemacs.org/emacs/elisp-xah-find-text.html")
